0a1e92ad11683390a3aa76f870863d802dd5bb76
MouadBH <mouad123b@gmail.com>
2020-03-28 00:29:18 +0000

    worldwide chart & add readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# coronapy-cli
+
+Track the Coronavirus disease (COVID-19) Strain & Saty Home.
+
+- 🚀 Get worldwide Coronavirus disease (COVID-19) reporting
+- 🤯 Active daily reporting of your country's COVID-19 statistics
+
+### Prerequisites
+
+```
+Python 3.7
+```
+
+## Contributing
+
+Give us a star, Fork repo & make a PR.
+
+## Authors
+
+* **MouadBH**
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
--- cli.py
+++ cli.py
@@ -1,29 +1,44 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import os
+import time
 import click
 import requests
 from lib import color, get_world_wide
 from pyfiglet import Figlet, figlet_format
 from prettytable import PrettyTable
+from yaspin import yaspin
+from termgraph import termgraph as tg
 
 @click.group()
 def cli():
     """A CLI wrapper for the API of Public APIs."""
     
 @cli.command()
-def start():
+@click.option('--draw', '-d', default=False,help='Draw a chart for the data.')
+def start(draw):
     """Start corona cli."""
-    print(chr(27) + "[2J")
+    click.clear()
     f = Figlet(font='slant')
     click.echo(str(color.prCyan(figlet_format('+----+----+----+----+----+----+', font = "digital"))))
     click.echo(str(color.prCyan(f.renderText('Corona CLI'))) + str(color.prGreen('Corona-cli')) + str(color.prYellow('v1.0.0')) + ' by MouadBH')
-    click.echo('Track the Coronavirus disease (COVID-19).')
-
+    click.echo(' Track the Coronavirus disease (COVID-19).')
+    
     worldwide_table = PrettyTable()
     worldwide_table.field_names = [color.prCyan('Cases'), color.prRed("Deaths"), color.prGreen("Recovered")]
-    worldwide_table.add_row(get_world_wide.world_wide())
+
+    with yaspin(text="Worldwide Cases", color="cyan") as sp:
+        worldwide_table.add_row([f'{member:,}' for member in get_world_wide.world_wide()])
+        time.sleep(2)    
+
     click.echo(worldwide_table)
+    print(' \n')
+    if draw:
+        labels = ['Cases', 'Deaths', 'Recovered']
+        data = [[i] for i in get_world_wide.world_wide()]
+        args = {'stacked': False, 'width': 50, 'no_labels': False, 'format': '{:<5.2f}',
+                'suffix': '', "vertical": False }
+        tg.chart(colors=[91, 94], data=data, args=args, labels=labels)
 
 if __name__ == '__main__':
     cli(prog_name='coronapy')

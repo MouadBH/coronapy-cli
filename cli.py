2f546dd5676c4162dd5a710d28a8851fea570e8e
MouadBH <mouad123b@gmail.com>
2020-03-28 18:44:09 +0000

    siplay a histogram for specific country

--- cli.py
+++ cli.py
@@ -5,7 +5,7 @@ import time
 import click
 import requests
 from lib import color, get_world_wide, get_countries, get_country
-from pyfiglet import Figlet, figlet_format
+from pyfiglet import Figlet
 from prettytable import PrettyTable
 from yaspin import yaspin
 from termgraph import termgraph as tg
@@ -13,7 +13,6 @@ from termgraph import termgraph as tg
 def init():
     click.clear()
     f = Figlet(font='slant')
-    #click.echo(str(color.prCyan(figlet_format('+---+----+----+----+----+---+', font = "digital"))))
     click.echo(str(color.prCyan(f.renderText('Corona CLI'))) + str(color.prGreen('Corona-cli')) + str(color.prYellow('v1.0.0')) + ' by MouadBH.')
     click.echo(' Track the Coronavirus disease (COVID-19).')
     print(' \n')
@@ -24,13 +23,13 @@ def cli():
     init()
 
 @cli.command()
-@click.option('--chart', '-c', is_flag=True, help='Draw a chart for the data.')
+@click.option('--chart', '-c', is_flag=True, help='Draw a chart of the data.')
 def start(chart):
-    """Start corona cli."""    
+    """Start coronapy cli."""    
     worldwide_table = PrettyTable()
     worldwide_table.field_names = [color.prCyan('Cases'), color.prRed("Deaths"), color.prGreen("Recovered")]
 
-    with yaspin(text="Worldwide Cases", color="cyan") as sp:
+    with yaspin(text="Worldwide Cases.", color="cyan") as sp:
         worldwide_table.add_row([f'{i:,}' for i in get_world_wide.world_wide()])
         time.sleep(1)    
 
@@ -61,7 +60,7 @@ def countries(sort):
         color.prPurple("Critical")
     ]
 
-    with yaspin(text="Worldwide Cases", color="cyan") as sp:
+    with yaspin(text="Civid-19 Cases Of All Countries", color="cyan") as sp:
         for country in get_countries.all_countries(sort):
             all_countries_table.add_row(country)
 
@@ -69,8 +68,10 @@ def countries(sort):
 
 @cli.command()
 @click.option('--chart', '-c', is_flag=True, help='Draw a chart for the data.')
+@click.option('--hist', '-h', is_flag=True, help='Draw a histogram for the data.')
+@click.option('--type', '-t', default='=cases',help='Type of historical data.')
 @click.argument('country')
-def country(country, chart):
+def country(country, chart, hist, type):
     """Get data of a specific country.""" 
     worldwide_table = PrettyTable()
     worldwide_table.field_names = [
@@ -84,21 +85,31 @@ def country(country, chart):
         color.prPurple("Critical")
     ]
 
-    with yaspin(text="Worldwide Cases", color="cyan") as sp:
+    with yaspin(text="Civid-19 Cases Of " + country, color="cyan") as sp:
         data = get_country.get_country(country)
         worldwide_table.add_row(data)
         time.sleep(1)    
 
     click.echo(worldwide_table)
-    print(' \n')
+    print(' ')
     
     if chart:
         labels = ['Total Cases', 'Total Deaths', 'Recovered Cases']
         data_chart = [data[1], data[3], data[5]]
         data_chart = [[int(i)] for i in data_chart]
-        args = {'stacked': False, 'width': 50, 'no_labels': False, 'format': '{:,}',
+        args = {'stacked': False, 'width': 100, 'no_labels': False, 'format': '{:,}',
                 'suffix': '', "vertical": False }
         tg.chart(colors=[91, 94], data=data_chart, args=args, labels=labels)
+
+    if hist:
+        with yaspin(text="Drawing a histogram of " + country, color="cyan") as sp:
+            labels_hist, hist_data = get_country.get_country_hist(country, type[1:])
+            time.sleep(1)
+
+        args = {'stacked': False, 'width': 100, 'no_labels': False, 'format': '{:,}',
+                'suffix': '', "vertical": False }
+        click.secho("Civid-19 '" + type[1:] + "' for last 20 day in " + country + ".", bg='black', fg='yellow', blink=True, bold=True)
+        tg.chart(colors=[91, 94], data=hist_data, args=args, labels=labels_hist)
 
 if __name__ == '__main__':
     cli(prog_name='coronapy')
--- lib/get_country.py
+++ lib/get_country.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 def get_country(country):
     url = 'https://corona.lmao.ninja/countries/' + country
@@ -14,3 +15,15 @@ def get_country(country):
         response.json()['critical']
     ]
     return data
+
+def get_country_hist(country, type):
+    url = 'https://corona.lmao.ninja/v2/historical/' + country
+    response = requests.get(url)
+    data = response.json()['timeline']
+    dicts = data[type]
+    labels = []
+    hist_data = []
+    for key, value in list(dicts.items())[-20:]:
+        labels.append(datetime.strptime(key, '%m/%d/%y').strftime('%d %b'))
+        hist_data.append([value])
+    return labels, hist_data

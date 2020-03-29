b017e6a556297928ca84588605085e0733a82b91
mlisovyi <mlisovyi@gmail.com>
2020-03-29 12:38:24 +0200

    remove trailing spaces

--- coronapy/lib/get_countries.py
+++ coronapy/lib/get_countries.py
@@ -7,14 +7,14 @@ def all_countries(sorted_by):
     i=1
     for country in response.json():
         allCountries.append([
-            i, 
-            country['country'], 
-            '{:,}'.format(country['cases']), 
-            '{:,}'.format(country['todayCases']), 
-            '{:,}'.format(country['deaths']), 
-            '{:,}'.format(country['todayDeaths']), 
-            '{:,}'.format(country['recovered']), 
-            '{:,}'.format(country['active']), 
+            i,
+            country['country'],
+            '{:,}'.format(country['cases']),
+            '{:,}'.format(country['todayCases']),
+            '{:,}'.format(country['deaths']),
+            '{:,}'.format(country['todayDeaths']),
+            '{:,}'.format(country['recovered']),
+            '{:,}'.format(country['active']),
             '{:,}'.format(country['critical'])
         ])
         i = i + 1

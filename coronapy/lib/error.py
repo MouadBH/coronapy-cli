ac31696733e69a9f05ef6f5abdbdedb882621858
OkiStuff <40648091+OkiStuff@users.noreply.github.com>
2020-06-05 21:34:14 -0400

    removed debugging stuff

--- coronapy/lib/error.py
+++ coronapy/lib/error.py
@@ -17,5 +17,5 @@ def crash_msg(error_code, file: str, fixes: str):
     pecho(color.prRed("There was an error which forced Coronapy to crash") )
     pecho(color.prGreen("But you can report this bug! https://github.com/MouadBH/coronapy-cli/issues") )
     pecho(color.prPurple("Before you report the bug, please try these fixes: " + fixes) )
-    pecho(color.prRed("ERROR Codesfdsfdasfsa: \n" + str(error_code) + "in file " + file + "\n You can put this error code in the bug report (it would help us out)") )
+    pecho(color.prRed("ERROR Code: \n" + str(error_code) + "in file " + file + "\n You can put this error code in the bug report (it would help us out)") )
 

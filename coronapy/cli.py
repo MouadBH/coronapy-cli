60555c849bafca132201a598adf21e9ff7e62470
okistuff <probro1802@gmail.com>
2020-06-10 09:32:13 -0400

    Added Twitch Link and Added some color to init()

--- coronapy/cli.py
+++ coronapy/cli.py
@@ -3,7 +3,7 @@
 import time
 import click
 import requests
-from lib import color, get_world_wide, get_countries, get_country
+from lib import color, get_world_wide, get_countries, get_country, error
 from pyfiglet import Figlet
 from prettytable import PrettyTable
 from yaspin import yaspin
@@ -15,7 +15,13 @@ def init():
     f = Figlet(font='slant')
     click.echo(str(color.prCyan(f.renderText('Corona CLI'))) + str(color.prGreen('Corona-cli')) + str(
         color.prYellow('v1.3.9')) + ' by MouadBH.')
-    click.echo(' Track the Coronavirus disease (COVID-19).')
+    click.echo(str(color.prPurple('Follow me on Twitch: https://www.twitch.tv/chibakutsy'))) 
+    
+
+
+        
+
+    click.echo(str(color.prRed('Track the Coronavirus disease (COVID-19).') ) )
     print(' \n')
 
 
